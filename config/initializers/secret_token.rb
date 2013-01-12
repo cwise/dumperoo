3f89df9fda9fe2e41af1600bb65f9b95f248a60d
Chris Wise <cwise@murmurinformatics.com>
2013-01-11 20:27:37 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dumperoo::Application.config.secret_token = '1c0633dd004d70d34517e10e49ae13785dd7e90cc70bf62f31f9516111b5193fcf4fe5c67dfadfe4452abb9410650f92ceb8751c3ff296a340c5ecfdbd429914'
